958bca20ad4aae2096acd37aff6203baac5c3eb3
0skarmpx <omoralespj@gmail.com>
2023-07-10 22:17:34 -0700

    print numbers plus', '

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(00, 100):
-    print("{:02d}".format(numbers), end="")
-    if numbers != 99:
-        print(", ", end="")
+for num in range(0, 100):
+    if num < 99:
+        print("{:02d}, ".format(num), end="")
+    else:
+        print("{:02d}".format(num))
