cc71399cd639ddd326183481c09bfe8dcc789e53
0skarmpx <omoralespj@gmail.com>
2023-07-10 22:56:03 -0700

    print numbers from 01 until 89

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        print("{:d}{:d}".format(i, j))
+for i in range(9):
+    for j in range(i+1, 10):
+        print("{:02d}, ".format(i * 10 + j), end="")
