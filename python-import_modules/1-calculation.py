af880becfabc15a14272a9230661f8e433933370
root <omoralespj@gmail.com>
2023-07-13 14:21:14 -0700

    operators in pyhton

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-result = add(a, b)
-result = sub(a, b)
-result = mul(a, b)
-result = div(a, b)
+    a = 10
+    b = 5
+    result = add(a, b)
+    result = sub(a, b)
+    result = mul(a, b)
+    result = div(a, b)
 print("{} + {} = {}".format(a, b, result))
 print("{} - {} = {}".format(a, b, result))
 print("{} * {} = {}".format(a, b, result))
