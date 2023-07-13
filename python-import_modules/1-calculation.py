5cc554ba606696ef0ad98c7ab61a07e758628aff
root <omoralespj@gmail.com>
2023-07-13 14:25:43 -0700

    operators in pyhton

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    result = add(a, b)
-    result = sub(a, b)
-    result = mul(a, b)
-    result = div(a, b)
-print("{} + {} = {}".format(a, b, result))
-print("{} - {} = {}".format(a, b, result))
-print("{} * {} = {}".format(a, b, result))
-print("{} / {} = {}".format(a, b, result))
+    result_1 = add(a, b)
+    result_2 = sub(a, b)
+    result_3 = mul(a, b)
+    result_4 = div(a, b)
+print("{} + {} = {}".format(a, b, result_1))
+print("{} - {} = {}".format(a, b, result_2))
+print("{} * {} = {}".format(a, b, result_3))
+print("{} / {} = {}".format(a, b, result_4))
