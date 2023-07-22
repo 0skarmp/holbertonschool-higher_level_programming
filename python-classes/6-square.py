1636a1e685a47cdb3c026935bfce6682996f642a
0skarmp <omoralespj@gmail.com>
2023-07-22 13:43:19 -0700

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,63 +4,59 @@ This module defines an empty class Square that represents a square.
 """
 
 
-class square:
+class Square:
     """
     This is the Square class.
     It represents a square with private attributes.
     """
-    
 
     def __init__(self, size=0, position=(0, 0)):
         """
         Initializes a new instance of the Square class.
         Args:
             size (int): The size of the square.
+            position (int): the position of the square.
         """
-        self._size = size
-        self._position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
-        return self._size
+        return self.__size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self._size = value
-
+        self.__size = value
+        
     @property
     def position(self):
-        return self._position
+        return self.__position
 
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        for i in value:
-            if i <= 0:
-                raise ValueError("position must be a tuple of 2 positive integers")
-        self._position = value
-
+        x, y = value
+        if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+            raise TypeError("position must be a touple of 2 positive integers")
+        self.__position = value
+    
     def area(self):
-        return self._size * self._size
+        return self.__size ** 2
 
     def my_print(self):
-        if self._size == 0:
+        if self.__size == 0:
             print("")
         else:
-            for i in range(self._size):
-                for _ in range(self.position[1]):
-                    print(" ", end="")
-                for j in range(self._size):
-                    if i == 0 or i == self._size - 1 or j == 0 or j == self._size - 1:
-                        print("#", end="")
-                    else:
-                        if self.position[0] > 0:
-                            print(" ", end="")
-                        if self.position[1] > 0:
-                            print(" ", end="")
+            for post0 in range(self.__position[1]):
                 print("")
+            for column in range(self.__size):
+              for colum in range(self.__position[0]):
+                print(" ", end="")
+            for reglones in range(self.__size):
+                print ("#", end="")
+        print()
