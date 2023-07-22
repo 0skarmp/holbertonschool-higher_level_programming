9898f79fc6ddac73a9556cb3f496ee9bf198037b
0skarmp <omoralespj@gmail.com>
2023-07-22 14:15:27 -0700

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
         return self.__position
@@ -42,21 +42,21 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         x, y = value
         if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
-            raise TypeError("position must be a touple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-    
+
     def area(self):
         return self.__size ** 2
 
     def my_print(self):
         if self.__size == 0:
-            print("")
+            print()
         else:
             for post0 in range(self.__position[1]):
-                print("")
+                print()
             for column in range(self.__size):
-              for colum in range(self.__position[0]):
-                print(" ", end="")
-            for reglones in range(self.__size):
-                print ("#", end="")
-        print()
+                for colum in range(self.__position[0]):
+                    print(" ", end="")
+                for reglones in range(self.__size):
+                    print("#", end="")
+                print()
