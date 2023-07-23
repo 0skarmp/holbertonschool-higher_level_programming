33e57a7fa2be5ba01ff6714fecb24230ad6a3e11
0skarmp <omoralespj@gmail.com>
2023-07-22 23:31:53 -0700

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     numbers = 0
     for i in range(x):
         try:
-            print(my_list[1], end="")
+            print(my_list[i], end="")
             numbers += 1
-        except TypeError:
+        except IndexError:
             break
     print()
     return numbers
