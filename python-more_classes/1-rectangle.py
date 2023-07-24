5de131357bb900dc53fa074b5a346447bd5af549
0skarmp <omoralespj@gmail.com>
2023-07-24 15:03:42 -0700

    task1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("[3~width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
