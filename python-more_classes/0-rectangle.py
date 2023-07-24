33f9db579f15341b52a96ff68e8892a762fc7af4
0skarmp <omoralespj@gmail.com>
2023-07-24 14:27:04 -0700

    task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+this module define a empty class rectangle.
+"""
+
 
 class Rectangle:
     """
-    Empty class Rectangle
+    This is class Rectangle without attrubutes.
     """
