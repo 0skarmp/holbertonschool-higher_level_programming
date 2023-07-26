b1decf29c4276b42e8efd7c35b9eaeebd52e4669
0skarmp <omorales@gmail.com>
2023-07-26 16:49:35 -0700

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this is module define a sub class.
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    this is a method
+    """
+    if isinstance(obj, a_class):
+        if type(obj) is a_class:
+            return False
+        return isinstance(obj, a_class)
