136e3634e7f5c8ae9c893fb37ff8a26847cc241a
root <0skarmp>
2023-07-26 17:21:21 -0700

    task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,5 +5,8 @@ this module define a class geometry
 
 
 class BaseGeometry:
+    """
+    this class define a method
+    """
     def area(self):
         raise Exception ("area() is not implemented")
