563dae40577a3c90d272720d4f31fc5c9e5c4672
root <0skarmp>
2023-07-27 08:03:26 -0700

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,7 +9,6 @@ def inherits_from(obj, a_class):
     """
     this is a method
     """
-    if isinstance(obj, a_class):
-        if type(obj) is a_class:
-            return False
-        return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)        
