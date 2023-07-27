277eee7f836707ee2ec42f07ff4bc75b8be36bc0
root <0skarmp>
2023-07-26 23:02:04 -0700

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this module define a class geometry
+"""
+
+
+class BaseGeometry:
+    """
+    this class define a method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    this class is inherits of BaseGeometry.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
