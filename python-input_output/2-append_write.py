c58d34f14a061afb469a96b90189cbaa74249f2e
0skarmp <omoralespj@gmail.com>
2023-07-29 23:39:11 -0700

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,8 @@ this function define 'appends'
 
 
 def append_write(filename="", text=""):
+    """
+    this is a method that add text
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
