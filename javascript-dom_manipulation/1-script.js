2fdf8b2719295e53862740ce3842623d8249ac88
OSKAR <omoralespj@gmail.com>
2023-10-14 13:28:40 -0500

    task1, change color at the click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,9 @@
+let headerTitle = document.querySelector("header");
+
+headerTitle.addEventListener("click", function() {
+    this.style.color = "#FF0000";
+});
+
+
+// let  hedaderTtitle = document.querySelector("header").addEventListener("click", function(){
+//     hedaderTtitle.style.color = "#FF0000";});
