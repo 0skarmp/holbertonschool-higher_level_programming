ac88c1bee3d172d6ae5e34901779708726d308d1
OSKAR <omoralespj@gmail.com>
2023-10-14 16:24:49 -0500

    taks 02

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+let headerTitle = document.getElementById("red_header");
+let headerElement = document.querySelector("header");
+
+headerTitle.addEventListener("click", function() {
+    headerElement.classList.add("red");
+});
+
