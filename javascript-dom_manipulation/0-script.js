e0b62e82058d775cd570393669dacf260f147a76
OSKAR <omoralespj@gmail.com>
2023-10-14 16:26:17 -0500

    task 1

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+let  hedaderTtitle = document.querySelectorAll("header")[0];
+hedaderTtitle.style.color = "#FF0000"
+
+let  hedaderTtitle = document.getElementsByTagName("header")  ;
+// hedaderTtitle.style.color = "#FF0000"
