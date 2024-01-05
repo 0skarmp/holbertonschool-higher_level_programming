866a00800891ebe6ee4b667eacf04d30502b35c7
0skarmp <omoralespj@gmail.com>
2024-01-05 16:37:05 +0000

    task 1, using function abs for take the las digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 LastDigit = abs(number) % 10
 print(f"last digit of {number} is {LastDigit}",end=" ")
 if LastDigit > 5:
-    print("is greater than 5")
+    print("and is greater than 5")
 elif LastDigit == 0:
     print ("and is 0")
-else :
-    print("and less than 6 and not 0")   
+else:
+    print("and is less than 6 and not 0")
