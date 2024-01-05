19a91ebd9216f0385d5c7144a5ca05bb47a30eaa
0skarmp <omoralespj@gmail.com>
2024-01-05 17:14:28 +0000

    add codition for negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-LastDigit = number % 10
+LastDigit = number % 10 if number >= 0 else -(-number % 10)
 print(f"Last digit of {number} is {LastDigit}",end=" ")
 if LastDigit > 5:
     print("and is greater than 5")
