39ddfe7e91d16b3fbe381ca8ed900fc9eabb74cb
0skarmp <omoralespj@gmail.com>
2024-01-05 22:21:16 +0000

    task 5 numbers by 2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{:02}".format(number), end="," if number < 99 else "\n")
+    print("{:02}".format(number), end=", " if number < 99 else "\n")
