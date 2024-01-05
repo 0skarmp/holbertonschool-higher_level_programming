c2361cb4fd61969ec172e2140729773cd3706bb3
0skarmp <omoralespj@gmail.com>
2024-01-05 19:00:33 +0000

    use f string in ascci

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 for ascci in range(97, 123):
-    print("{:c}".format(ascci), end="")
+    """print("{:c}".format(ascci), end="")"""
+    print("{}".format(chr(ascci)), end="")
     """print(chr(ascci), end="")"""
 print()
