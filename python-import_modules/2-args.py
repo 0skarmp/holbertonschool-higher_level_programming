ba95049769a85d321aa7711338fb32f3feb349c1
0skarmp <omoralespj@gmail.com>
2024-01-07 06:13:44 +0000

    fix bugs, spaces

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif argument == 1:
         print("1 argument:")
     else:
-        print("{} : arguments:".format(argument))
+        print("{} arguments:".format(argument))
     for i in range(argument):
-        print("{} : {}".format(i+1, sys.argv[i+1]))
+        print("{}: {}".format(i+1, sys.argv[i+1]))
