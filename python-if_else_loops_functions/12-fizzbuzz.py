e8b49e00e33b799912374b87e4333cfa4aad38a5
0skarmp <omoralespj@gmail.com>
2024-01-07 02:09:21 +0000

    fix bug, space

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("fizz", end=" ")
         elif i % 5 == 0:
-            print("buzz ", end=" ")
+            print("buzz ", end="")
         else:
             print(i, end=" ")
 
