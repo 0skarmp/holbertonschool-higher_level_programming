b61c4aea8fdc202665cfb14bac1a8a1bad72f687
OSKAR <omoralespj@gmail.com>
2024-01-20 17:36:51 -0500

    task 11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Script to list all the records with a score >= 10
 
-SELECT score, name FROM second_table ORDER BY score >=10 DESC;
+SELECT score, name FROM second_table WHERE score >=10 ORDER BY DESC;
