2ca48bdb5efe91154df551d2028014ff947d842f
OSKAR <omoralespj@gmail.com>
2024-01-20 18:16:55 -0500

    task 101

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- SCRIPT 
 
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUPBY CITY ORDER BY avg_temp DESC;
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
